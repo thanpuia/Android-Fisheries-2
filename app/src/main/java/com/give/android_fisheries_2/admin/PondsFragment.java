d8965fb081f0fb43dcb950540789e3faf7adbe7d
lalthanpuia <thanpuia46@gmail.com>
2020-05-20 22:32:25 +0530

    map added. but the fragment can be called only once...

--- app/src/main/java/com/give/android_fisheries_2/admin/PondsFragment.java
+++ app/src/main/java/com/give/android_fisheries_2/admin/PondsFragment.java
@@ -4,16 +4,23 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.give.android_fisheries_2.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PondsFragment extends Fragment {
+public class PondsFragment extends Fragment implements OnMapReadyCallback {
 
     public PondsFragment() {
         // Required empty public constructor
@@ -24,6 +31,49 @@ public class PondsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ponds, container, false);
+        View view =null;//= inflater.inflate(R.layout.fragment_ponds, container, false);
+
+        if (view != null) {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if (parent != null)
+                parent.removeView(view);
+        }
+        try {
+            view = inflater.inflate(R.layout.fragment_ponds, container, false);
+        } catch (InflateException e) {
+
+            Log.d("TAG","CARCH");
+
+           // return inflater.inflate(R.layout.fragment_ponds, container, false);
+                 MapFragment mapFragment = (MapFragment) getActivity().getFragmentManager()
+                            .findFragmentById(R.id.maps);
+                    if(mapFragment==null)
+                        mapFragment.getMapAsync(this);
+
+
+        }
+
+
+        MapFragment mapFragment = (MapFragment) getActivity().getFragmentManager()
+                .findFragmentById(R.id.maps);
+        if(mapFragment==null)
+            mapFragment.getMapAsync(this);
+
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
     }
 }
