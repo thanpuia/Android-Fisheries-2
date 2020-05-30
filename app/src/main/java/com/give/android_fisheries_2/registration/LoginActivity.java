5bb7683207ba1d10f2cdda70b3c8146f36f43876
lalthanpuia <thanpuia46@gmail.com>
2020-05-30 11:59:19 +0530

    registration and login error handling

--- app/src/main/java/com/give/android_fisheries_2/registration/LoginActivity.java
+++ app/src/main/java/com/give/android_fisheries_2/registration/LoginActivity.java
@@ -29,7 +29,7 @@ import es.dmoral.toasty.Toasty;
 public class LoginActivity extends AppCompatActivity {
 
     public SharedPreferences sharedPreferences;
-    MaterialEditText loginEmail,loginPassword;
+    MaterialEditText loginContact,loginPassword;
     LinearLayout loginLinearLayout;
     LinearLayout simpleProgressBarLinearLayout;
     Button loginButton;
@@ -41,83 +41,77 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginEmail = findViewById(R.id.loginContact);
+        loginContact = findViewById(R.id.loginContact);
         loginPassword = findViewById(R.id.loginPassword);
         loginLinearLayout = findViewById(R.id.loginFormLinearLayout);
         //loginProgressBar = findViewById(R.id.simpleProgressBarLogin);
         simpleProgressBarLinearLayout = findViewById(R.id.simpleProgressBarLinearLayout);
         loginButton = findViewById(R.id.loginInButton);
 
+
     }
 
     public void mLoginButtonClick(View view) {
 
-        String mLoginContact = loginEmail.getText().toString();
-        String mLoginPassword = loginPassword.getText().toString();
+        String mLoginContact ;
+        String mLoginPassword ;
 
-        // ::THIS SHOW THE PROGRESS BAR LAYOUT AND MAKE THE LOGIN BUTTON DISABLE SO THAT USER WILL NOT CONSTANTLY PUSH THE BUTTON ::
-        simpleProgressBarLinearLayout.setVisibility(View.VISIBLE);
-        loginButton.setEnabled(false);
-
-        //loginLinearLayout.setVisibility(View.INVISIBLE);
-
-        Ion.with(getApplicationContext())
-                .load("http://192.168.43.205:8000/api/login")
-/*                .uploadProgressHandler(new ProgressCallback() {
-                    @Override
-                    public void onProgress(long downloaded, long total) {
-                        loginLinearLayout.setVisibility(View.INVISIBLE);
-                        loginProgressBar.setVisibility(View.VISIBLE);
-                    }
-                })*/
-                .setMultipartParameter("contact",mLoginContact)
-                .setMultipartParameter("password",mLoginPassword)
-                .asJsonObject()
-                .setCallback(new FutureCallback<JsonObject>() {
-                    @Override
-                    public void onCompleted(Exception e, final JsonObject result) {  /* NOTE: Login Success   result{  "success":"true" }     Login Unsuccess  result{   "success":"false"  }  */
-                        //::::HIDE THE PROGRESS BAR AND MAKE THE BUTTON ENABLE AGAIN
-                        simpleProgressBarLinearLayout.setVisibility(View.INVISIBLE);
-                        loginButton.setEnabled(true);
-
-                        Log.e("TAG","TESTING: "+result.get("success").getAsBoolean());
-                        Log.e("TAG","result: "+result);
-
-                        boolean success = result.get("success").getAsBoolean();
-                        String mToken = result.get("token").getAsString();
-                        String mName = result.get("name").getAsString();
-                        String mContact = result.get("contact").getAsString();
-                   //     String mRole = result.get("role").getAsString();
-
-                        int mId = result.get("id").getAsInt();
-
-
-                        Log.e("TAG","TESTING: "+result.get("success"));
-
-                        Log.e("TAG","result:"+result);
-                        //THIS GET AS BOOLEAN IS VERY IMP
-                        if(result.get("success").getAsBoolean()==true){
-
-                            sharedPreferences = getApplication().getSharedPreferences("com.example.root.sharedpreferences", Context.MODE_PRIVATE);
-                            sharedPreferences.edit().putBoolean("mLoginStatus",true).apply();
-                            sharedPreferences.edit().putInt("mId",mId).apply();
-                            sharedPreferences.edit().putString("mName",mName).apply();
-                            sharedPreferences.edit().putString("mToken",mToken).apply();
-                            sharedPreferences.edit().putString("mContact",mContact).apply();
-                          //  sharedPreferences.edit().putString("mRole",mRole).apply();
-
-
-                            Log.e("TAG","LOGIN");
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                            intent.putExtra("status","good");
-                            startActivity(intent);
-                            finish();
-                        }else{
-
-                            Toasty.error(getApplicationContext(),"Incorrect Input",Toasty.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+        //::::BOTH FIELDS ARE EMPTY
+        if( loginContact.getText().toString().matches("") &&   loginPassword.getText().toString().matches("")){
+            loginContact.setError("Please enter contact details!");
+            loginPassword.setError("Please enter password!");
+        }else{
+            //::::ONE OF THE FIELD IS MISSING
+            if(loginContact.getText().toString().matches("") )
+                loginContact.setError("Please enter contact details!");
+            else if(loginPassword.getText().toString().matches(""))
+                loginPassword.setError("Please enter password!");
+            else {
+                mLoginPassword = loginPassword.getText().toString();
+                mLoginContact = loginContact.getText().toString();
+                // ::THIS SHOW THE PROGRESS BAR LAYOUT AND MAKE THE LOGIN BUTTON DISABLE SO THAT USER WILL NOT CONSTANTLY PUSH THE BUTTON ::
+                simpleProgressBarLinearLayout.setVisibility(View.VISIBLE);
+                loginButton.setEnabled(false);
+                Ion.with(getApplicationContext())
+                        .load("http://192.168.43.205:8000/api/login")
+                        .setMultipartParameter("contact",mLoginContact)
+                        .setMultipartParameter("password",mLoginPassword)
+                        .asJsonObject()
+                        .setCallback(new FutureCallback<JsonObject>() {
+                            @Override
+                            public void onCompleted(Exception e, final JsonObject result) {  /* NOTE: Login Success   result{  "success":"true" }     Login Unsuccess  result{   "success":"false"  }  */
+                                //::::HIDE THE PROGRESS BAR AND MAKE THE BUTTON ENABLE AGAIN
+                                simpleProgressBarLinearLayout.setVisibility(View.INVISIBLE);
+                                loginButton.setEnabled(true);
+                                if(result==null){
+                                    Toasty.error(getApplicationContext(),"Incorrect Input",Toasty.LENGTH_SHORT).show();
+                                }else{
+                                    Log.e("TAG","TESTING: "+result.get("success").getAsBoolean());
+                                    Log.e("TAG","result: "+result);
+                                    String mToken = result.get("token").getAsString();
+                                    String mName = result.get("name").getAsString();
+                                    String mContact = result.get("contact").getAsString();
+                                    String mRole = result.get("role").getAsString();
+                                    int mId = result.get("id").getAsInt();
+                                    //THIS GET AS BOOLEAN IS VERY IMP
+                                    if(result.get("success").getAsBoolean()==true){
+                                        sharedPreferences = getApplication().getSharedPreferences("com.example.root.sharedpreferences", Context.MODE_PRIVATE);
+                                        sharedPreferences.edit().putBoolean("mLoginStatus",true).apply();
+                                        sharedPreferences.edit().putInt("mId",mId).apply();
+                                        sharedPreferences.edit().putString("mName",mName).apply();
+                                        sharedPreferences.edit().putString("mToken",mToken).apply();
+                                        sharedPreferences.edit().putString("mContact",mContact).apply();
+                                        sharedPreferences.edit().putString("mRole",mRole).apply();
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        intent.putExtra("status","good");
+                                        startActivity(intent);
+                                        finish();
+                                    }else Toasty.error(getApplicationContext(),"Incorrect Input",Toasty.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        }
     }
 
     public void notYetRegisterClick(View view) {
--- app/src/main/java/com/give/android_fisheries_2/registration/RegistrationActivity.java
+++ app/src/main/java/com/give/android_fisheries_2/registration/RegistrationActivity.java
@@ -21,20 +21,18 @@ import com.koushikdutta.ion.Ion;
 import com.koushikdutta.ion.ProgressCallback;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
+import java.util.StringJoiner;
+
 import es.dmoral.toasty.Toasty;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private MaterialEditText name, password, phone;
+    private MaterialEditText name, password, contact, passwordConfirm;
     private Button registerNowButton;
     private ProgressBar progressBar;
     private SharedPreferences sharedPreferences;
     RelativeLayout registrationProgressBarRelativeLayout;
-
-    private String URLs= String.valueOf(R.string.IP_ADDRESS);
-    String mContact;
-    String mName;
-
+    String mName, mContact, mPassword, mPasswordConfirm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,88 +40,76 @@ public class RegistrationActivity extends AppCompatActivity {
 
         name = findViewById(R.id.userName);
         password = findViewById(R.id.userPassword);
-        phone = findViewById(R.id.userPhone);
-
+        passwordConfirm = findViewById(R.id.userPasswordConfirm);
+        contact = findViewById(R.id.userPhone);
         registerNowButton = findViewById(R.id.registerNowButton);
         progressBar = findViewById(R.id.simpleProgressBarRegistration);
         registrationProgressBarRelativeLayout = findViewById(R.id.registration_progress_bar_relative_layout);
-
     }
 
     public void registerNowClick(View view) {
 
-        //:::: SHOW THE PROGRESS BAR AND DISABLE THE REGISTRATION BUTTON ::::
-        registrationProgressBarRelativeLayout.setVisibility(View.VISIBLE);
-        registerNowButton.setEnabled(false);
+        mName = name.getText().toString();
+        mContact = contact.getText().toString();
+        mPassword = password.getText().toString();
+        mPasswordConfirm = passwordConfirm.getText().toString();
 
-        //Toasty.info(this,"REGISTER click",Toasty.LENGTH_SHORT).show();
-        try{
-            mName = name.getText().toString();
-            String mPassword = password.getText().toString();
-            mContact = phone.getText().toString();
+        if(mName.matches("") && mPassword.matches("") && mContact.matches("")&& mPasswordConfirm.matches("")){
+            name.setError("Please enter your name!");
+            contact.setError("Please enter contact details!");
+            password.setError("Please enter password!");
+            passwordConfirm.setError("Please enter confirm password!");
+        }
 
-            Ion.with(getApplicationContext())
-                    .load("http://192.168.43.205:8000/api/register")
-//                    .uploadProgressHandler(new ProgressCallback() {
-//                        @Override
-//                        public void onProgress(long downloaded, long total) {
-//                            progressBar.setVisibility(View.VISIBLE);
-//                            registerNowButton.setVisibility(View.INVISIBLE);
-//                        }
-//                    })
-                    .setMultipartParameter("name",mName)
-                    .setMultipartParameter("contact",mContact)
-                    .setMultipartParameter("password",mPassword)
-                    .asJsonObject()
-                    .setCallback(new FutureCallback<JsonObject>() {
-                        @Override
-                        public void onCompleted(Exception e, JsonObject result) {
+        if(mName.matches(""))   name.setError("Please enter contact details!");
+        else if(mContact.matches("")) contact.setError("Please enter contact details!");
+        else if(mPassword.matches(""))  password.setError("Please enter password!");
+        else if(mPasswordConfirm.matches(""))   passwordConfirm.setError("Please enter confirm password!");
+        else if(mContact.length() != 10) Toasty.error(this,"Phone number should be 10 digits",Toasty.LENGTH_SHORT).show();
+        else if(!mPassword.matches(mPasswordConfirm)) Toasty.error(this,"Password not matching! ",Toasty.LENGTH_SHORT).show();
+        else{
+            //:::: SHOW THE PROGRESS BAR AND DISABLE THE REGISTRATION BUTTON ::::
+            registrationProgressBarRelativeLayout.setVisibility(View.VISIBLE);
+            registerNowButton.setEnabled(false);
+            try{
+                Ion.with(getApplicationContext())
+                        .load("http://192.168.43.205:8000/api/register")
+                        .setMultipartParameter("name",mName)
+                        .setMultipartParameter("contact",mContact)
+                        .setMultipartParameter("password",mPassword)
+                        .asJsonObject()
+                        .setCallback(new FutureCallback<JsonObject>() {
+                            @Override
+                            public void onCompleted(Exception e, JsonObject result) {
+                                registrationProgressBarRelativeLayout.setVisibility(View.INVISIBLE);
+                                registerNowButton.setEnabled(true);
+                                Log.e("TAG","RESULT::"+result);
+                                if(result!=null){
+                                    JsonElement tokenAsJson = result.get("token");
+                                    Log.e("TAG","TOKEN::"+result.get("token"));
+                                    if(tokenAsJson!=null){
+                                        Toasty.success(getApplicationContext(),"Register Successfully!",Toasty.LENGTH_SHORT).show();
+                                        //STORED IN THE SHARED PREFERENCE FOR LATER REFERENCE
+                                        sharedPreferences = getApplication().getSharedPreferences("com.example.root.sharedpreferences", Context.MODE_PRIVATE);
+                                        sharedPreferences.edit().putString("mToken",result.get("token").toString()).apply();
+                                        sharedPreferences.edit().putString("mName",mName).apply();
+                                        sharedPreferences.edit().putString("mContact",mContact).apply();
+                                        progressBar.setVisibility(View.INVISIBLE);
+                                        registerNowButton.setVisibility(View.VISIBLE);
 
-                            registrationProgressBarRelativeLayout.setVisibility(View.INVISIBLE);
-                            registerNowButton.setEnabled(true);
-
-                            Log.e("TAG","RESULT::"+result);
-                            if(result!=null){
-                              //  JsonElement statusAsJson = result.get("success");
-                                JsonElement tokenAsJson = result.get("token");
-
-                                Log.e("TAG","RESULT::"+result.get("success"));
-                                Log.e("TAG","TOKEN::"+result.get("token"));
-
-
-                                if(tokenAsJson!=null){
-                                    Toasty.success(getApplicationContext(),"Register Successfully!",Toasty.LENGTH_SHORT).show();
-
-                                    //STORED IN THE SHARED PREFERENCE FOR LATER REFERENCE
-                                    sharedPreferences = getApplication().getSharedPreferences("com.example.root.sharedpreferences", Context.MODE_PRIVATE);
-                                    sharedPreferences.edit().putString("mToken",result.get("token").toString()).apply();
-
-                                    //GET THE ROLE FROM THE SERVER
-                                    sharedPreferences.edit().putString("mRole","FARMER").apply();
-
-                                    //sharedPreferences.edit().putString("mRole","ADMIN").apply();
-                                    sharedPreferences.edit().putString("mName",mName).apply();
-                                    sharedPreferences.edit().putString("mContact",mContact).apply();
-                                    Log.e("TAG","RESULT::"+result);
-                                    startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
-                                    progressBar.setVisibility(View.INVISIBLE);
-                                    registerNowButton.setVisibility(View.VISIBLE);
-                                }else{
-                                    Toasty.error(getApplicationContext(),"Email already taken or password should be more than 8 char",Toasty.LENGTH_SHORT).show();
-                                }
-                            }else{
-                                Toasty.error(getApplicationContext(),"Error!",Toasty.LENGTH_SHORT).show();
+                                        startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+                                    }else Toasty.error(getApplicationContext(),"Phone number already taken or password should be more than 8 char",Toasty.LENGTH_SHORT).show();
+                                }else Toasty.error(getApplicationContext(),"Phone number already taken or password should be more than 8 char",Toasty.LENGTH_SHORT).show();
                             }
-
-                        }
-                    });
-        }catch (Exception e){}
-
+                        });
+            }catch (Exception e){}
+        }
     }
 
     public void loginInTextClick(View view) {
         startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
     }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
